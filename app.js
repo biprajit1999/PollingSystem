8943219f4f841a7dcf932b9d0d339c866cde4d5c
biprajit1999 <biprajit1999@gmail.com>
2023-04-07 02:38:38 +0530

    project updated

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+const loginRouter = require('./login2');
+const formRouter = require('./form'); 
+// const loginRouter2 = require('./login');
+
+app.use(loginRouter);
+app.use(formRouter);
+
+app.listen(3000, () => console.log('Server started'));
---
+++ login2.js
@@ -0,0 +1,62 @@
+var express=require("express");
+var bodyParser=require("body-parser");
+const app = express.Router();
+
+const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost:27017/placement3');
+mongoose.connect('mongodb+srv://biprajit:biprajit@cluster0.has27be.mongodb.net/placement_sortlist?retryWrites=true&w=majority');
+
+var db=mongoose.connection;
+db.on('error', console.log.bind(console, "connection error"));
+db.once('open', function(callback){
+	console.log("connection succeeded");
+})
+
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+
+app.post('/login', function(req,res){
+	var univid = req.body.univid;
+    var password = req.body.password;
+
+
+	var data = {
+		"univid":univid,
+        "password": password
+	}
+
+db.collection('login').findOne({ univid: univid }, function(err, user) {
+
+
+    if (err) {
+        res.status(500).send('Internal server error');
+        return;
+      }
+      if (!user) {
+        res.status(401).send('User not found');
+        return;
+        }
+      if (user.password !== password) {
+        res.status(401).send('Invalid password');
+        return;
+      }
+      return res.redirect('form.html');
+    });
+});
+
+
+app.get('/',function(req,res){
+    res.set({
+        'Access-control-Allow-Origin': '*'
+        });
+    return res.redirect("index.html");
+    
+    })
+
+
+module.exports = app;
